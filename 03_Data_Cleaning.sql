39af1961b3eb802cfaa6586ea1da3f65908f20bf
ojaswani16 <ojaswani.sharma16@gmail.com>
2025-06-09 01:02:40 +0530

    Update 03_Data_Cleaning.sql

--- 03_Data_Cleaning.sql
+++ 03_Data_Cleaning.sql
@@ -1,15 +1,15 @@
--- Inspect Data Structure
+-- Inspect the Data Structure
 SELECT * 
 FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`
 LIMIT 10;
 
--- Check for duplicate ride_id
+-- Check for Duplicate ride_id
 SELECT ride_id, COUNT(ride_id)
 FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`
 GROUP BY ride_id
 HAVING COUNT(ride_id) > 1;
 
--- Count total duplicate rows
+-- Count Total Duplicate Rows
 SELECT COUNT(*) AS total_duplicate_rows
 FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`
 WHERE ride_id IN (
@@ -19,7 +19,7 @@ WHERE ride_id IN (
   HAVING COUNT(*) > 1
 );
 
--- Trimming whitespace from station names
+-- Trim Whitespace from Station Names
 CREATE OR REPLACE TABLE `cyclistic-analysis-461216.cyclistic_2024_data.cleaned_rides_2024` AS
 SELECT 
   *,
@@ -27,14 +27,45 @@ SELECT
   TRIM(end_station_name) AS end_station_name
 FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`;
 
--- Removing NULL station names 
+-- Remove Rows with NULL Start Station Data
 CREATE OR REPLACE TABLE `cyclistic-analysis-461216.cyclistic_2024_data.analysis_cyclistic` AS
 SELECT *
 FROM `cyclistic-analysis-461216.cyclistic_2024_data.cleaned_rides_2024`
 WHERE start_station_name IS NOT NULL
   AND start_station_id IS NOT NULL;
 
--- Removing Invalid Durations
+-- Remove Invalid Ride Durations
 DELETE FROM `cyclistic-analysis-461216.cyclistic_2024_data.cleaned_rides_2024`
 WHERE trip_duration <= 0;
 
+-- Row Count
+SELECT COUNT(*) 
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`;
+
+-- NULL Value Check
+SELECT 
+  COUNTIF(start_station_name IS NULL) AS null_start_station,
+  COUNTIF(end_station_name IS NULL) AS null_end_station
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`;
+
+-- Member Type Distribution
+SELECT member_casual, COUNT(*) AS ride_count
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`
+GROUP BY member_casual;
+
+-- Rideable Type Distribution
+SELECT rideable_type, COUNT(*) AS ride_count
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.rides_2024`
+GROUP BY rideable_type;
+
+-- Add Ride Hour
+CREATE OR REPLACE TABLE `cyclistic-analysis-461216.cyclistic_2024_data.analysis_with_hour` AS
+SELECT *,
+  EXTRACT(HOUR FROM start_time) AS ride_hour
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.analysis_cyclistic`;
+
+-- Add Month Number
+CREATE OR REPLACE TABLE `cyclistic-analysis-461216.cyclistic_2024_data.analysis_with_hour` AS
+SELECT *,
+  EXTRACT(MONTH FROM PARSE_DATE('%B', month)) AS month_num
+FROM `cyclistic-analysis-461216.cyclistic_2024_data.analysis_with_hour`;
